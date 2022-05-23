41e8a368ca7998a1665f6ff0ceb04b66cec37a74
clmn386 <clmn386@gmail.com>
2022-05-23 21:08:14 +0200

    .

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -10,7 +10,31 @@ class ActiveRecord {
     // Alerta y Mensajes 
     protected static $alertas = [];
 
-    
+    // Definir la conexion a la base de datos
+    public static function setDB($database) {
+        self::$db = $database;
+    }
+
+    public static function setAlerta($tipo, $mensaje){
+        static::$alertas[$tipo][] = $mensaje;
+    }
+
+    // Validacion
+    public static function getAlert(){
+        return static::$alertas;
+    }
+
+    public function validar(){
+        static::$alertas = [];
+        return static::$alertas;
+    }
+
+    // Consultar SQL para crear un objeto en Memoria
+    public static function consultarSQL($query){
+        // Consultar la base de datos
+        $resultado = self::$db->query($query);
+        
+    }
 }
 
 ?>
