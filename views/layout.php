67ef79d283f39d23e5f4b2610924275446183016
clmn386 <clmn386@gmail.com>
2022-05-27 09:18:20 +0200

    scss

--- views/layout.php
+++ views/layout.php
@@ -5,14 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portafolio</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,500;0,700;1,200;1,500;1,700&display=swap" rel="stylesheet"> 
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,500;0,700;1,200;1,500;1,700&display=swap" rel="stylesheet">  -->
     <link rel="stylesheet" href="../public/build/css/app.css">
 </head>
 <body>
     <div class="base">
-        <p>hola</p>
+        <h1>HOLA Mundo portafolio</h1>
+        <h2>Test de fuentes</h2>
+        <h3>Para conocer si todo va bien</h3>
+        <p>En el panel derecho, haz clic en Opciones avanzadas. En el cuadro de texto, pega el código de Google para establecer reglas CSS para esa fuente. En la parte inferior derecha, haz clic en Siguiente. Luego haz clic en Guardar.</p>
     </div>
+
 </body>
 </html>
