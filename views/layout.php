c44c0ad598e63f49989d8b312ec0cbb2920ea4ed
clmn386 <clmn386@gmail.com>
2022-05-30 22:35:18 +0200

    .

--- views/layout.php
+++ views/layout.php
@@ -8,13 +8,31 @@
     <!-- <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,500;0,700;1,200;1,500;1,700&display=swap" rel="stylesheet">  -->
     <link rel="stylesheet" href="../public/build/css/app.css">
 </head>
-<body>
-    <div class="base">
+<body class="body">
+    <div class="banner">
+        <img src="../src/img/cyberpunk-2077.png" alt="">
+    </div>
+    <div class="barra">
+        <nav>
+            <ul>Home</ul>
+            <ul>info</ul>
+            <ul>examples</ul>
+            <ul>contact</ul>
+        </nav>
+    </div>
+    <div class="contenido">
         <h1>HOLA Mundo portafolio</h1>
         <h2>Test de fuentes</h2>
         <h3>Para conocer si todo va bien</h3>
         <p>En el panel derecho, haz clic en Opciones avanzadas. En el cuadro de texto, pega el código de Google para establecer reglas CSS para esa fuente. En la parte inferior derecha, haz clic en Siguiente. Luego haz clic en Guardar.</p>
+
+        <input type="input" class="boton" value="boton">
     </div>
 
 </body>
+<footer class="footer">
+ <p class="footer_contenido">
+     Derechos Reservados &#169: Carlos Luis Mata Noguera, 2022
+ </p>
+</footer>
 </html>
