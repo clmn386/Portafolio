d1f804cf243b9c71982f98bc19c018b3139f1b0a
clmn386 <clmn386@gmail.com>
2022-05-31 23:00:36 +0200

    .

--- views/layout.php
+++ views/layout.php
@@ -14,21 +14,64 @@
     </div>
     <div class="barra">
         <nav>
-            <ul>Home</ul>
-            <ul>info</ul>
-            <ul>examples</ul>
-            <ul>contact</ul>
+            <a href="#">Home</a>
+            <a href="#">Info</a>
+            <a href="#">Proyectos</a>
+            <a href="#">contactar</a>
         </nav>
     </div>
     <div class="contenido">
-        <h1>HOLA Mundo portafolio</h1>
-        <h2>Test de fuentes</h2>
-        <h3>Para conocer si todo va bien</h3>
-        <p>En el panel derecho, haz clic en Opciones avanzadas. En el cuadro de texto, pega el código de Google para establecer reglas CSS para esa fuente. En la parte inferior derecha, haz clic en Siguiente. Luego haz clic en Guardar.</p>
+        <div class="home">
+            <div class="home-contenedor">
+                <h2>Test de fuentes</h2>
+                <h3>Para conocer si todo va bien</h3>
+                <p>En el panel derecho, haz clic en Opciones avanzadas. En el cuadro de texto, pega el código de Google para establecer reglas CSS para esa fuente. En la parte inferior derecha, haz clic en Siguiente. Luego haz clic en Guardar.</p>
+            </div>
+            <div class="home-contenedor_info">
+                <div>
+                    <img src="../src/img/cyberpunk-player.png" alt="imagen juego">
+                </div>
+                <div>
+                    <p>
+                        En el juego el jugador será arrojado a este oscuro futuro. La metrópolis de Night City es un lugar creado para contarnos la historia de un individuo que creció en sus calles y que trata de levantarse y encontrar un modo de sobrevivir entre las mafias locales y las megacorporaciones, en una ciudad de suciedad y decadencia.
+                    </p>
+                    <p>
+                        Drogas, violencia, pobreza y exclusión no han desaparecido en el año 2077 y la gente ha seguido estando como lo era hace siglos, egoísta, de mente cerrada y débil. Pero no sólo los fantasmas del pasado preocupan a la humanidad, sino que nuevos problemas han aparecido. Los psicópatas se acumulan, y las calles están llenas de adictos a una nueva forma de entretenimiento: el Braindance, una forma barata de experimentar las emociones y el estímulo de otras personas que viven una vida más emocionante.      
+                    </p>
 
-        <input type="input" class="boton" value="boton">
+                </div>
+
+            </div>
+            <input type="input" class="boton" value="boton">
+            
+        </div>
+        <div class="contenido-iconos">
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-css3" width="60" height="60" viewBox="0 0 24 24" stroke-width="1" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z" />
+                <path d="M8.5 8h7l-4.5 4h4l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5" />
+                </svg>
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-php" width="60" height="60" viewBox="0 0 24 24" stroke-width="1" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <ellipse cx="12" cy="12" rx="10" ry="9" />
+                <path d="M5.5 15l.395 -1.974l.605 -3.026h1.32a1 1 0 0 1 .986 1.164l-.167 1a1 1 0 0 1 -.986 .836h-1.653" />
+                <path d="M15.5 15l.395 -1.974l.605 -3.026h1.32a1 1 0 0 1 .986 1.164l-.167 1a1 1 0 0 1 -.986 .836h-1.653" />
+                <path d="M12 7.5l-1 5.5" />
+                <path d="M11.6 10h2.4l-.5 3" />
+                </svg>
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-sass" width="60" height="60" viewBox="0 0 24 24" stroke-width="1" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="12" cy="12" r="9" />
+                <path d="M12 10.523c2.46 -.826 4.002 -.826 4.002 -2.155c0 -1.366 -1.347 -1.366 -2.735 -1.366c-1.91 0 -3.352 .49 -4.537 1.748c-.848 .902 -1.027 2.449 -.153 3.307c.973 .956 3.206 1.789 2.884 3.493c-.233 1.235 -1.469 1.823 -2.617 1.202c-.782 -.424 -.454 -1.746 .626 -2.512s2.822 -.992 4.1 -.24c.98 .575 1.046 1.724 .434 2.193" />
+                </svg>
+            </div>
+        </div>
     </div>
-
 </body>
 <footer class="footer">
  <p class="footer_contenido">
